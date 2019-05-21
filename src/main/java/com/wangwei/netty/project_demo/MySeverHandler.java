950778bc075e0218ed4ef45d46acf365beee9f74
wangwei <wangwei@risingstarinfo.com>
2019-05-21 18:39:22 +0800

    测试分支提交

---
+++ src/main/java/com/wangwei/netty/project_demo/MySeverHandler.java
@@ -0,0 +1,18 @@
+package com.wangwei.netty.project_demo;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @Author: wangwei
+ * @Date: 2019/5/21 18:28
+ * @Version 1.0
+ */
+public class MySeverHandler extends SimpleChannelInboundHandler {
+
+    //这个
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+
+    }
+}
